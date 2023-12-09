6ae314f3143c37b9d1a5d96fbdb11497bfc9e30a
nabeel-ncz <nabeelnabhan115@gmail.com>
2023-12-09 18:00:32 +0530

    Latest build 09-12-23, URL changed

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 connectDb();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }
